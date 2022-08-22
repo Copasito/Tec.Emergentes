56a659af5e6502a86b41b1c9043d51d78c00f339
Jorge Figueroa Soto <jafjaveriana@javerianacali.edu.co>
2022-08-22 16:28:14 -0500

    FailTest

--- test_main.py
+++ test_main.py
@@ -11,5 +11,5 @@ def test_index_route():
 
 def test_read_main():
     response = client.get("/")
-    assert response.status_code == 400
-    #assert response.json() == {"items": "Hello World"}
+    assert response.status_code == 200
+    assert response.json() == {"items": "Hello World"}
