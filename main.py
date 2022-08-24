c9426809a475a03c37a4372c05d51323dac323f1
Jorge Figueroa Soto <jafjaveriana@javerianacali.edu.co>
2022-08-24 08:30:30 -0500

    main mod

--- main.py
+++ main.py
@@ -13,33 +13,32 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
+header = {'Content-type': 'application/json', 'Accept': 'Application/json'}
+url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
+#url = 'https://6303e5400de3cd918b3fde59.mockapi.io/items' getitem put post
 @app.get("/")
-def read_root():
-    url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
+def read_root():   
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json() }
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    response = requests.get(url+'/'+str(item_id), timeout=5)
+    return {"item": response.json()}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    requests.put(url+"/"+str(item_id), item.json(), headers=header)
+    return "Se añadio el nuevo producto"
 
-@app.post("/items/{item_id}")###################
+@app.post("/items/{item_id}")
 def upload_item(item_id: int, item: Item):
-    #url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
-    #obj = {'item_name': item.name}
-    #requests.post(url, json= obj)
-    return {"item_name": item.name, "item_id": item_id}
+    requests.post(url, item.json(), headers=header)
+    return "Se añadio el nuevo producto"
 
-@app.delete("/items/")########################
-def delete_item():
-    url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
-    response = requests.delete(url)
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    response = requests.delete(url+"/"+str(item_id))
     return {"items": response.json() }
 
 
